c6f24aefd72848ba6a173aaec865d210a675a59a
DarcyManoel <darcman00@gmail.com>
2024-08-02 09:03:56 +0930

    Clean instant_crafting code

--- instant_crafting_1.0.0/control.lua
+++ instant_crafting_1.0.0/control.lua
@@ -1,55 +1,48 @@
-script.on_event(defines.events.on_pre_player_crafted_item, function(event)
-    local player = game.players[event.player_index]
-    InstantCrafting_InitGlobal()
-    InstantCrafting_CancelCraft(player)
-    InstantCrafting_RemoveIngredients(player)
-    InstantCrafting_AddProducts(player)
+script.on_event(defines.events.on_pre_player_crafted_item,function(event)
+	local player=game.players[event.player_index]
+	instantCrafting_initGlobal()
+	instantCrafting_cancelCraft(player)
+	instantCrafting_removeIngredients(player)
+	instantCrafting_addProducts(player)
 end)
-
-script.on_event(defines.events.on_player_cancelled_crafting, function(event)
-    local player = game.players[event.player_index]
-    InstantCrafting_RememberProducts(event.recipe, event.cancel_count)
-    InstantCrafting_RememberIngredients(event.items)
+script.on_event(defines.events.on_player_cancelled_crafting,function(event)
+	local player=game.players[event.player_index]
+	instantCrafting_rememberProducts(event.recipe,event.cancel_count)
+	instantCrafting_rememberIngredients(event.items)
 end)
-
-function InstantCrafting_InitGlobal()
-    global.InstantCrafting = global.InstantCrafting or {}
-    global.InstantCrafting.products = {}
-    global.InstantCrafting.ingredients = {}
+function instantCrafting_initGlobal()
+	global.instantCrafting=global.instantCrafting or {}
+	global.instantCrafting.products={}
+	global.instantCrafting.ingredients={}
 end
-
-function InstantCrafting_CancelCraft(player)
-    local craftToCancel = {index = #player.crafting_queue}
-    if #player.crafting_queue > 0 then
-        craftToCancel.count = player.crafting_queue[craftToCancel.index].count
-        player.cancel_crafting(craftToCancel)
-    end
+function instantCrafting_cancelCraft(player)
+	local craftToCancel={index=#player.crafting_queue}
+	if #player.crafting_queue>0 then
+		craftToCancel.count=player.crafting_queue[craftToCancel.index].count
+		player.cancel_crafting(craftToCancel)
+	end
 end
-
-function InstantCrafting_RememberProducts(recipe, count)
-    for _, product in pairs(recipe.products) do
-        local productToInsert = {name = product.name}
-        productToInsert.count = (product.amount or 1) * count
-        table.insert(global.InstantCrafting.products, productToInsert)
-    end
+function instantCrafting_rememberProducts(recipe,count)
+	for _,product in pairs(recipe.products) do
+		local productToInsert={name=product.name}
+		productToInsert.count=(product.amount or 1)*count
+		table.insert(global.instantCrafting.products,productToInsert)
+	end
 end
-
-function InstantCrafting_AddProducts(player)
-    for _, product in pairs(global.InstantCrafting.products) do
-        player.insert(product)
-    end
+function instantCrafting_addProducts(player)
+	for _, product in pairs(global.instantCrafting.products) do
+		player.insert(product)
+	end
 end
-
-function InstantCrafting_RememberIngredients(items)
-    global.InstantCrafting.ingredients = {}
-    for item, count in pairs(items.get_contents()) do
-        local stack = {name = item, count = count}
-        table.insert(global.InstantCrafting.ingredients, stack)
-    end
+function instantCrafting_rememberIngredients(items)
+	global.instantCrafting.ingredients={}
+	for item,count in pairs(items.get_contents()) do
+		local stack={name=item,count=count}
+		table.insert(global.instantCrafting.ingredients,stack)
+	end
 end
-
-function InstantCrafting_RemoveIngredients(player)
-    for _, ingredient in pairs(global.InstantCrafting.ingredients) do
-        player.remove_item(ingredient)
-    end
+function instantCrafting_removeIngredients(player)
+	for _,ingredient in pairs(global.instantCrafting.ingredients) do
+		player.remove_item(ingredient)
+	end
 end
