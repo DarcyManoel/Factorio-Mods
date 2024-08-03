c0aabb3aa440bcecf088885d1cffd8942e2e159c
DarcyManoel <darcman00@gmail.com>
2024-08-03 16:27:40 +0930

    Replace Enhanced Deconstruction

---
+++ instant_deconstruction_1.0.0/control.lua
@@ -0,0 +1,8 @@
+script.on_event(defines.events.on_player_deconstructed_area,function(event)
+	local player=game.players[event.player_index]
+	local marked_entities=game.surfaces[1].find_entities_filtered{to_be_deconstructed=1}
+	for _,entity in pairs(marked_entities) do
+		player.mine_entity(entity)
+		game.print(entity)
+	end
+end)
