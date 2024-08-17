e940de03d64cbac58f5f05a2e790dc9bac9453e6
DarcyManoel <darcman00@gmail.com>
2024-08-17 16:01:08 +0930

    Update Mod: instant_deconstruction

--- instant_deconstruction_1.0.1/control.lua
+++ instant_deconstruction_1.0.1/control.lua
@@ -3,6 +3,5 @@ script.on_event(defines.events.on_player_deconstructed_area,function(event)
 	local marked_entities=game.surfaces[1].find_entities_filtered{to_be_deconstructed=1}
 	for _,entity in pairs(marked_entities)do
 		player.mine_entity(entity)
-		game.print(entity)
 	end
 end)
