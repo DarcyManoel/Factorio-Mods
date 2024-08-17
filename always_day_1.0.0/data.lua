bcec80086a1045a56d4f0c742d548ec9486d8de4
DarcyManoel <darcman00@gmail.com>
2024-08-17 16:00:14 +0930

    Create Mod: always_day

---
+++ always_day_1.0.0/data.lua
@@ -0,0 +1,8 @@
+data.raw['utility-constants']['default'].daytime_color_lookup={
+	{0.200,'identity'},
+	{0.340,'identity'},
+	{0.450,'identity'},
+	{0.550,'identity'},
+	{0.660,'identity'},
+	{0.800,'identity'}
+}
