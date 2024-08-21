1eb017be234315783cb40fa8c78c6bf9a846f588
DarcyManoel <darcman00@gmail.com>
2024-08-21 23:09:55 +0930

    Update Mod: far_reach

--- far_reach_1.0.1/data.lua
+++ far_reach_1.0.1/data.lua
@@ -2,4 +2,3 @@ data.raw['character']['character'].build_distance=1000
 data.raw['character']['character'].drop_item_distance=1000
 data.raw['character']['character'].reach_distance=1000
 data.raw['character']['character'].reach_resource_distance=1000
-data.raw['character']['character'].loot_pickup_distance=1000
